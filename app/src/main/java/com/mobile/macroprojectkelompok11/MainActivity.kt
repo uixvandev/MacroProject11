52859cc075f60d03d6b24d0be3274aa7348dcb01
irfan wahendra <2111527002_irfan@student.unand.ac.id>
2023-10-17 18:14:06 +0700

    add bottomNav

--- app/src/main/java/com/mobile/macroprojectkelompok11/MainActivity.kt
+++ app/src/main/java/com/mobile/macroprojectkelompok11/MainActivity.kt
@@ -2,23 +2,40 @@ package com.mobile.macroprojectkelompok11
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.denzcoskun.imageslider.ImageSlider
-import com.denzcoskun.imageslider.constants.ScaleTypes
-import com.denzcoskun.imageslider.models.SlideModel
+import androidx.fragment.app.Fragment
+import com.mobile.macroprojectkelompok11.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(FragmentHome())
 
-        val imageSlider = findViewById<ImageSlider>(R.id.imageSlider)
-        val imageList = ArrayList<SlideModel>()
+        binding.bottomNav.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.homeItem -> replaceFragment(FragmentHome())
+                R.id.mapsItem -> replaceFragment(FragmentMaps())
 
-        imageList.add(SlideModel("https://images.unsplash.com/photo-1597165826924-f0e9fb81762e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80","Kesehatanmu adalah aset berharga. Teruslah peduli dengan kualitas udara di sekitarmu!"))
-        imageList.add(SlideModel("https://images.unsplash.com/photo-1611273426858-450d8e3c9fce?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fHBvbGx1dGlvbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60","Jaga kesehatanmu dengan baik, karena kualitas udara yang bersih adalah kunci untuk hidup yang lebih baik dan berkualitas!"))
-        imageList.add(SlideModel("https://images.unsplash.com/photo-1616943412395-5ecf65f437d1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2841&q=80","Saat kamu memperhatikan kualitas udara di sekitarmu, kamu juga menjaga kesehatanmu yang tak ternilai."))
+                else -> {
+
+                }
+            }
+            true
+        }
 
 
-        imageSlider.setImageList(imageList, ScaleTypes.FIT, )
+    }
+
+
+
+    //    Memanggil fragment ke frame_layout di activity_main
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.content, fragment)
+        fragmentTransaction.commit()
     }
 }
