92e3da8503b0005bc674cf661c8dba71c4e4e7a9
Randi Pratama <puterapratama11@gmail.com>
2023-10-19 05:06:16 +0700

    Perbaikan LoginActivityRandi, SignUpActivityRandi

---
+++ app/src/main/java/com/mobile/macroprojectkelompok11/SignUpActivityRandi.kt
@@ -0,0 +1,11 @@
+package com.mobile.macroprojectkelompok11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivityRandi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_randi)
+    }
+}
