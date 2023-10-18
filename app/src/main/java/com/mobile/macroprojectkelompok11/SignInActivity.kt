eab32c4119a6c6518b5c09af6190ac3a8f114dd0
Yassar Muwwafaq Malyan Shabira <yassaricha@gmail.com>
2023-10-18 19:09:12 +0700

    tess

--- app/src/main/java/com/mobile/macroprojectkelompok11/SignInActivity.kt
+++ app/src/main/java/com/mobile/macroprojectkelompok11/SignInActivity.kt
@@ -1,27 +1,54 @@
 package com.mobile.macroprojectkelompok11
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
 
 class SignInActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var editusername : EditText
+    private lateinit var editpassword : EditText
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
+        //mendapatkan data username dan password dari register
+        editusername = findViewById(R.id.edt_username)
+        editpassword = findViewById(R.id.edt_reg_password)
+        val bundle = intent.extras
+        if(bundle !=null){
+            editusername.setText(bundle.getString("username"))
+            editpassword.setText(bundle.getString("password"))
+        }
+
         val btnSignIn : Button = findViewById(R.id.btn_signin)
         btnSignIn.setOnClickListener(this)
+
+        val signUpTextView = findViewById<TextView>(R.id.linkto_signup)
+        signUpTextView.setOnClickListener {
+
+            val myIntent = Intent(this, SignUpActivity::class.java)
+            startActivity(myIntent)
+
+            signUpTextView.movementMethod = LinkMovementMethod.getInstance()
+
+        }
+
 
     }
 
     override fun onClick(v: View) {
         when(v.id) {
             R.id.btn_signin -> {
-                val intent = Intent(this@SignInActivity, MainActivity::class.java)
+                val intent = Intent(this@SignInActivity, Izin::class.java)
                 startActivity(intent)
             }
 
--- app/src/main/java/com/mobile/macroprojectkelompok11/SignUpActivity.kt
+++ app/src/main/java/com/mobile/macroprojectkelompok11/SignUpActivity.kt
@@ -6,18 +6,30 @@ import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SignUpActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var editusername : EditText
+    private lateinit var editpassword : EditText
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        editusername = findViewById(R.id.edt_username)
+        editpassword = findViewById(R.id.edt_reg_password)
+        val bundle = intent.extras
+        if(bundle != null){
+            editusername.setText(bundle.getString("username"))
+            editpassword.setText(bundle.getString("password"))
+        }
+
         val btnRegister: Button = findViewById(R.id.btn_regist)
         btnRegister.setOnClickListener(this)
 
-        val signInTextView = findViewById<TextView>(R.id.link_signIn)
+        val signInTextView = findViewById<TextView>(R.id.linkto_signin)
         signInTextView.setOnClickListener {
 
             val myIntent = Intent(this, SignInActivity::class.java)
@@ -31,7 +43,12 @@ class SignUpActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View) {
         when(v.id){
             R.id.btn_regist-> {
+                val bundle = Bundle()
+                bundle.putString("username", editusername.text.toString())
+                bundle.putString("password", editpassword.text.toString())
+
                 val intent = Intent(this@SignUpActivity, SignInActivity::class.java)
+                intent.putExtras(bundle)
                 startActivity(intent)
             }
         }
