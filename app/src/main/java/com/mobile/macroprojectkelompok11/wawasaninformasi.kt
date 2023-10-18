268f0a2d70c3a602aa5f0607031636e8ff04c281
irfan wahendra <2111527002_irfan@student.unand.ac.id>
2023-10-18 19:48:18 +0700

    yeayy

--- app/src/main/java/com/mobile/macroprojectkelompok11/wawasaninformasi.kt
+++ app/src/main/java/com/mobile/macroprojectkelompok11/wawasaninformasi.kt
@@ -3,11 +3,7 @@ package com.mobile.macroprojectkelompok11
 import androidx.fragment.app.Fragment
 
 
-/**
- * A simple [Fragment] subclass.
- * Use the [wawasaninformasi.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class wawasaninformasi : Fragment(R.layout.fragment_wawasaninformasi) {
 
 }
